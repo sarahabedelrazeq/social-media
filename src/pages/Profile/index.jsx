9b63fa4492ffe6977d7351cb0d79d646538f23f8
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2022-10-08 21:06:49 +0300

    done

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -41,7 +41,55 @@ export default function Profile() {
   return (
     <div id="profile-page" className="page-container">
       <sections>
-
+        {loading || !user.id ? (
+          <Grid>
+            <Skeleton variant="text" height={300} marginBottom="200px" />
+            <Skeleton variant="text" height={20} />
+            <Skeleton variant="text" height={20} />
+            <Skeleton variant="rectangular" height={300} />
+            <Skeleton variant="text" height={20} />
+            <Skeleton variant="text" height={20} />
+            <Skeleton variant="rectangular" height={300} />
+          </Grid>
+        ) : (
+          <Grid>
+            <Grid>
+              <Box m="-16px" mb={0}>
+                <img src="/images/bg1.jpg" alt={user.name} className="w-100" height="300" />
+              </Box>
+              <Box
+                marginTop="-110px"
+                className="d-flex justify-content-center mb-3"
+              >
+                <Box
+                  sx={{
+                    padding: "10px",
+                    borderRadius: "50%",
+                    backgroundColor: "gray",
+                  }}
+                >
+                  <img
+                    src={user.image}
+                    alt={user.name}
+                    width="200"
+                    height="200"
+                    className="rounded-circle"
+                  />
+                </Box>
+              </Box>
+              <Box className="mb-5">
+                <Typography variant="h5" component="h2" className="text-center">
+                  {user.name}
+                </Typography>
+              </Box>
+              <Box>
+                {posts.map((post, index) => (
+                  <Post {...post} user={user} key={index} />
+                ))}
+              </Box>
+            </Grid>
+          </Grid>
+        )}
       </sections>
     </div>
   );
