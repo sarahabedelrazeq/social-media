3ca0c5c5f01bfeb6d3d5009fb58d9fd5e3d777e0
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2022-10-30 08:39:21 +0200

    done

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,6 +1,5 @@
 import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
 import {
-  Avatar,
   Card,
   CardActions,
   CardContent,
@@ -13,50 +12,53 @@ import {
 const Post = ({ image, text, time, user }) => {
   return (
     <div>
-      <Card sx={{ marginBottom: "24px" }}>
-        <CardHeader
-          avatar={
-            <img
-              src={user.image}
-              width={50}
-              height={50}
-              className="rounded-circle"
-            />
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVert />
-            </IconButton>
-          }
-          title={user.name}
-          subheader={time}
-        />
-        {image && (
-          <CardMedia
-            component="img"
-            height="20%"
-            image={image}
-            alt="Paella dish"
+      {user && (
+        <Card sx={{ marginBottom: "24px" }}>
+          <CardHeader
+            avatar={
+              <img
+                src={user.image}
+                width={50}
+                height={50}
+                className="rounded-circle"
+                alt={user.name}
+              />
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVert />
+              </IconButton>
+            }
+            title={user.name}
+            subheader={time}
           />
-        )}
-
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            {text}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <Checkbox
-              icon={<FavoriteBorder />}
-              checkedIcon={<Favorite sx={{ color: "red" }} />}
+          {image && (
+            <CardMedia
+              component="img"
+              height="20%"
+              image={image}
+              alt={text.slice(0, 30)}
             />
-          </IconButton>
-          <IconButton aria-label="share">
-            <Share />
-          </IconButton>
-        </CardActions>
-      </Card>
+          )}
+
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              {text}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <Checkbox
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite sx={{ color: "red" }} />}
+              />
+            </IconButton>
+            <IconButton aria-label="share">
+              <Share />
+            </IconButton>
+          </CardActions>
+        </Card>
+      )}
     </div>
   );
 };
